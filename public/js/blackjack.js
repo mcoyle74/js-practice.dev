65d8cb83dd1a4ad1b73aa4a26f79d7a8fe2dc602
Michael Coyle <mcoyle74@gmail.com>
2017-04-06 16:32:45 -0500

    Replaced suit variables with and array using html character names. Created a function to build the deck from the suits and ranks arrays and another to select a random card. Added button listener to deal the cards.

--- public/js/blackjack.js
+++ public/js/blackjack.js
@@ -1,10 +1,34 @@
 'use strict';
 
-var spades = String.fromCharCode(9824),
-	clubs = String.fromCharCode(9827),
-	hearts = String.fromCharCode(9829),
-	diamonds = String.fromCharCode(9830);
+// var spades = String.fromCharCode(9824),
+// 	clubs = String.fromCharCode(9827),
+// 	hearts = String.fromCharCode(9829),
+// 	diamonds = String.fromCharCode(9830);
 
-var $rank = $('.card-rank');
+var suits = ['&clubs;', '&spades;', '&hearts;', '&diams;'];
+var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+var cards = $('.card-rank');
 
-$rank[0].innerHTML = 'A' + '<br>' + spades;
+function buildDeck(suits, ranks) {
+	var deck = [];
+	suits.forEach(function(suit, index) {
+		ranks.forEach(function(rank) {
+			if (index < 2) {
+				deck.push(rank + '<br><span class="suit">' + suit + '</span>');
+			} else {
+				deck.push('<span class="red">' + rank + '</span><br>' + '<span class="suit red">' + suit + '</span>');
+			}
+		});
+	});
+
+	return deck;
+}
+
+function randomCard() {
+	return Math.floor(Math.random() * 52);
+}
+
+$('#deal').click(function() {
+	cards[0].innerHTML = buildDeck(suits, ranks)[randomCard()];
+	cards[1].innerHTML = buildDeck(suits, ranks)[randomCard()];
+});
