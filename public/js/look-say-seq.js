b096740bc5da981bbe97fc7de1e15e5a80da87af
Michael Coyle <mcoyle74@gmail.com>
2017-05-23 23:18:52 -0500

    Added an if statement to the beginning and a recursive function call to the return statement, lookAndSaySequence(next, n -1), so as to generate the nth term of the sequence.
    Solved: Create a function that takes in a string of digits and an integer n, and with the string as the initial element, return the nth term of the look-and-say sequence:
    	http://mathworld.wolfram.com/LookandSaySequence.html

--- public/js/look-say-seq.js
+++ public/js/look-say-seq.js
@@ -1,11 +1,15 @@
 "use strict";
 
 /*
-	Create a function that takes in a string of digits and an integer n and returns the nth term of the look-and-say sequence.
+	Create a function that takes in a string of digits and an integer n, and with the string as the initial element, return the nth term of the look-and-say sequence:
 	http://mathworld.wolfram.com/LookandSaySequence.html
 */
 
-function lookAndSaySequence(firstElement, n){
+function lookAndSaySequence(firstElement, n) {
+
+	if (n == 1) {
+		return firstElement;
+	}
 	
 	var digits = firstElement.split(''),
 		chunks = [];
@@ -28,5 +32,5 @@ function lookAndSaySequence(firstElement, n){
 		next += String(arr[i].length) + chunk[0];
 	});
 
-	return next;
+	return lookAndSaySequence(next, n - 1);
 }
