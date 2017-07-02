4304a473166833f7b6f39d1b24c732b9ec13d584
Michael Coyle <mcoyle74@gmail.com>
2017-07-02 14:45:28 -0500

    Moved and edited the funtions from pangram.js to this file.

--- public/js/word-play.js
+++ public/js/word-play.js
@@ -19,3 +19,58 @@ String.prototype.isPalindrome = function() {
 
 	return candidate === key ? true : false;
 }
+
+String.prototype.isPangram = function() {
+
+	if (this.valueOf().length < 26) {
+		return false;
+	}
+
+	var alphabet = getAlphabet(),
+		matchedCharCount = 0;
+
+	for (var i = 0; i < alphabet.length; i++) {
+		if (this.valueOf().includes(alphabet[i]) || this.valueOf().includes(alphabet[i].toUpperCase())) {
+			matchedCharCount++;
+		}
+	}
+
+	return (matchedCharCount == 26) ? true : false;
+}
+
+String.prototype.getMissing = function() {
+
+	var alphabet = getAlphabet().split(''),
+		string = this.valueOf(),
+		missing = alphabet.filter(letter => {
+			return !string.includes(letter) && !string.includes(letter.toUpperCase())
+		});
+
+	return missing.join(', ');
+}
+
+function getAlphabet() {
+
+	var alphabet = '';
+
+	for (var i = 97; i < 123; i++) {
+		alphabet += String.fromCharCode(i);
+	}
+
+	return alphabet;
+}
+
+var pangramTestStrings = [
+	"abcdefghijklmnopqrstuvwxyz",
+	"ab_defghijkl_nopqrstuvwxyz",
+	"Bold crisp qwerty juxtaposition",
+	"dfXrC]egbvxJ|2P:lmwk/C_*YAzCQ<",
+	"A quick brown fox jumps over the lazy dog",
+	"Pack my box with five dozen liquor jugs",
+	"Jack loves my big sphinx of quartz",
+	"The five boxing wizards jump quickly",
+	"Black quartz Sphinx, judge my vow",
+	"How vexingly quick daft zebras jump!",
+	"Grumpy wizards make a toxic brew for the jovial queen",
+	"Sixty zips quickly picked from the woven jute bag"
+];
