c5c5559f7e2522da37b851d4e401865636479929
Michael Coyle <mcoyle74@gmail.com>
2017-07-07 19:29:21 -0500

    Board now adjusts according to screen size: HTML - moved the row and column markers inside the #board div CSS - added media query to adjust board to 50% screen width for desktop 	- changed all the px dimensions to % 	- used some code from CSS Tricks to ensure the board remains a square as it's scaled JS - removed token property from board array elements so the whole objects become part of the array 	- white and black pieces now the same character

--- public/js/chess.js
+++ public/js/chess.js
@@ -35,7 +35,8 @@ function Game() {
 
 	this.move = function(fromSq, toSq) {
 	
-		var from = {
+		var piece = $(fromSq),
+			from = {
 				row: this.rowTrans(fromSq[1]),
 				col: this.colTrans(fromSq[0])
 			},
@@ -45,7 +46,7 @@ function Game() {
 			};
 
 		this.board[to.row][to.col] = this.board[from.row][from.col];
-		this.board[from.row][from.col] = '_';	
+		this.board[from.row][from.col] = '';	
 		this.onMove = (this.onMove === 'white') ? 'black' : 'white';
 		this.display();
 	};
@@ -53,48 +54,48 @@ function Game() {
 	this.reset = function() {
 		this.board = [
 			[
-				this.set.black.rooks[0].token,
-			 	this.set.black.knights[0].token,
-				this.set.black.bishops[0].token,
-				this.set.black.queen.token,
-				this.set.black.king.token,
-				this.set.black.bishops[1].token,
-				this.set.black.knights[1].token,
-				this.set.black.rooks[1].token
+				this.set.black.rooks[0],
+			 	this.set.black.knights[0],
+				this.set.black.bishops[0],
+				this.set.black.queen,
+				this.set.black.king,
+				this.set.black.bishops[1],
+				this.set.black.knights[1],
+				this.set.black.rooks[1]
 			],
 			[
-				this.set.black.pawns[0].token,
-				this.set.black.pawns[1].token,
-				this.set.black.pawns[2].token,
-				this.set.black.pawns[3].token,
-				this.set.black.pawns[4].token,
-				this.set.black.pawns[5].token,
-				this.set.black.pawns[6].token,
-				this.set.black.pawns[7].token,
+				this.set.black.pawns[0],
+				this.set.black.pawns[1],
+				this.set.black.pawns[2],
+				this.set.black.pawns[3],
+				this.set.black.pawns[4],
+				this.set.black.pawns[5],
+				this.set.black.pawns[6],
+				this.set.black.pawns[7],
 			],
-			['_', '_', '_', '_', '_', '_', '_', '_'],
-			['_', '_', '_', '_', '_', '_', '_', '_'],
-			['_', '_', '_', '_', '_', '_', '_', '_'],
-			['_', '_', '_', '_', '_', '_', '_', '_'],
+			['', '', '', '', '', '', '', ''],
+			['', '', '', '', '', '', '', ''],
+			['', '', '', '', '', '', '', ''],
+			['', '', '', '', '', '', '', ''],
 			[
-				this.set.white.pawns[0].token,
-				this.set.white.pawns[1].token,
-				this.set.white.pawns[2].token,
-				this.set.white.pawns[3].token,
-				this.set.white.pawns[4].token,
-				this.set.white.pawns[5].token,
-				this.set.white.pawns[6].token,
-				this.set.white.pawns[7].token,
+				this.set.white.pawns[0],
+				this.set.white.pawns[1],
+				this.set.white.pawns[2],
+				this.set.white.pawns[3],
+				this.set.white.pawns[4],
+				this.set.white.pawns[5],
+				this.set.white.pawns[6],
+				this.set.white.pawns[7],
 			],
 			[
-				this.set.white.rooks[0].token,
-			 	this.set.white.knights[0].token,
-				this.set.white.bishops[0].token,
-				this.set.white.queen.token,
-				this.set.white.king.token,
-				this.set.white.bishops[1].token,
-				this.set.white.knights[1].token,
-				this.set.white.rooks[1].token
+				this.set.white.rooks[0],
+			 	this.set.white.knights[0],
+				this.set.white.bishops[0],
+				this.set.white.queen,
+				this.set.white.king,
+				this.set.white.bishops[1],
+				this.set.white.knights[1],
+				this.set.white.rooks[1]
 			],
 		]
 
@@ -102,10 +103,9 @@ function Game() {
 	};
 
 	this.display = function() {
-		this.board.forEach((row, i) => {
-			console.log(-(i - 8) + '  ' + row.join('  '));
+		this.board.forEach((row) => {
+			console.log(row);
 		});
-		console.log('   a  b  c  d  e  f  g  h');
 	};
 
 	this.rowTrans = function(row) {
@@ -132,22 +132,22 @@ function Piece(rank, color, square) {
 	switch (rank) {
 
 		case 'king':
-			this.token = String.fromCharCode((this.color === 'white') ? 9812 : 9818);
+			this.token = String.fromCharCode(9818);
 			break;
 		case 'queen':
-			this.token = String.fromCharCode((this.color === 'white') ? 9813 : 9819);
+			this.token = String.fromCharCode(9819);
 			break;
 		case 'rook':
-			this.token = String.fromCharCode((this.color === 'white') ? 9814 : 9820);
+			this.token = String.fromCharCode(9820);
 			break;
 		case 'bishop':
-			this.token = String.fromCharCode((this.color === 'white') ? 9815 : 9821);
+			this.token = String.fromCharCode(9821);
 			break;
 		case 'knight':
-			this.token = String.fromCharCode((this.color === 'white') ? 9816 : 9822);
+			this.token = String.fromCharCode(9822);
 			break;
 		case 'pawn':
-			this.token = String.fromCharCode((this.color === 'white') ? 9817 : 9823);
+			this.token = String.fromCharCode(9823);
 			break;
 		default:
 			this.token = null;
