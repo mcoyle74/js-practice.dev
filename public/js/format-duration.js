c057e4ef17178dad145e1ca95bb09ff29cc550a0
Michael Coyle <mcoyle74@gmail.com>
2017-08-17 22:01:31 -0500

    Solved: Create a function that takes an integer representing seconds and returns the time formatted in a comma-separated list of years, days, hours, minutes and seconds. The last separator should be just the word 'and' instead of a comma. Units for any quantity that equals 1 should be singular and those greater than 1 should be plural. Units whose quantity is zero should be omitted unless the initial input is 0, then return 'now'. Examples: 	input: 134822520 	output: "4 years, 100 days, 10 hours and 42 minutes"
    	input: 3600
    	output: "1 hour"

---
+++ public/js/format-duration.js
@@ -0,0 +1,36 @@
+'use strict';
+
+function formatDuration(seconds) {
+
+	const MINUTE = 60,
+    	HOUR = MINUTE * 60,
+		DAY = HOUR * 24,
+		YEAR = DAY * 365;
+
+	var duration = [];
+
+	function formatUnits(sec, timeConst, unit) {
+
+		var time = Math.floor(sec / timeConst);
+		time += time > 1 ? `${unit}s` : unit;
+		seconds %= timeConst;
+		return time;
+
+	}
+
+	if (seconds >= YEAR) duration.push(formatUnits(seconds, YEAR, ' year'));
+	
+	if (seconds >= DAY) duration.push(formatUnits(seconds, DAY, ' day'));
+	
+	if (seconds >= HOUR) duration.push(formatUnits(seconds, HOUR, ' hour'));
+	
+	if (seconds >= MINUTE) duration.push(formatUnits(seconds, MINUTE, ' minute'));
+
+	if (seconds > 0) {
+		seconds += seconds > 1 ? ' seconds' : ' second';
+		duration.push(seconds);
+	}
+
+	return duration.join(', ').replace(/,([^,]*)$/, ' and$1');
+
+}
