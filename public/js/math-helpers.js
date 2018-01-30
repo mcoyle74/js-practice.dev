657e3d848eddc2fe8f3e26886876a8e875b27d19
Michael Coyle <mcoyle74@gmail.com>
2018-01-29 21:32:02 -0600

    Refactored the factorize function, much more efficient

--- public/js/math-helpers.js
+++ public/js/math-helpers.js
@@ -41,10 +41,19 @@ function factorial(n) {
 function factorize(n) {
 
 	let factors = [],
-		half = n / 2;
+		nSqrt = Math.sqrt(n),
+		index;
 
-	for (let i = 2; i <= half; i++) {
+	for (let i = 2; i < nSqrt; i++) {
 		if (n % i == 0) factors.push(i);
+	}
+
+	index = factors.length - 1;
+
+	if (Number.isInteger(nSqrt)) factors.push(nSqrt);
+
+	for (let i = index; i >= 0; i--) {
+		factors.push(n / factors[i]);
 	}
 
 	return factors;
