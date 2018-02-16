5977ff07018ead4bcca9ac5e9e98b817b9ac2696
Michael Coyle <mcoyle74@gmail.com>
2018-02-16 01:32:13 -0600

    Solved: write a function that decomposes a given positive integer's factorial (n!) into its prime factors.

--- public/js/coding-challenge/2018/02/p4.js
+++ public/js/coding-challenge/2018/02/p4.js
@@ -11,3 +11,32 @@
 		(5! = 5 * 4 * 3 * 2 * 1 = 120)			output: 2^15 * 3^6 * 5^3 * 7^2 * 11 * 13 * 17
 		output: 2^3 * 3 * 5
 */
+
+function factorialToPrimeFactors(integer) {
+	// primeFactors() and factorial() are from ../../../math-helpers.js
+	let array = primeFactors(factorial(integer)), 
+		baseExponent = {},
+		factors = [],
+		factorsString;
+
+	array.forEach(value => {
+		if (baseExponent.hasOwnProperty(value)) {
+			baseExponent[value]++;
+		} else {
+			baseExponent[value] = 1;
+		}
+	});
+
+	for (let key in baseExponent) {
+
+		factorsString = key;
+
+		if (baseExponent[key] > 1) {
+			factorsString += `^${baseExponent[key]}`;
+		}
+
+		factors.push(factorsString);
+	}
+
+	return factors.join(' * ');
+}
